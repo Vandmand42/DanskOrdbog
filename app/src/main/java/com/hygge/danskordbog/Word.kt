b48fc1f0c77f49ee9b2eb7a79a530eabea738687
Vandmand42 <Vandmand42@github.com>
2021-06-28 00:50:21 +0200

    - Local Database Added

---
+++ app/src/main/java/com/hygge/danskordbog/Word.kt
@@ -0,0 +1,11 @@
+package com.hygge.ordbog_interface
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
+
+//@ColumnInfo(name = "word") val word: String,
+//@PrimaryKey(autoGenerate = true) val id: Int
